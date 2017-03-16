6cb7babfb29f857ac7a9208ad70b3165d41a50c5
Lars Winderling <lars.winderling@performance-advertising.de>
2017-03-16 11:00:19 +0100

    Add script for bootstrapping.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
 set -euo pipefail
 
-GIT_DIR="~/git"
-VENV_DIR="~/venvs"
+BIN_DIR="${HOME}/bin"
+GIT_DIR="${HOME}/git"
+VENV_DIR="${HOME}/venvs"
+ANSIBLE_DIR="${VENV_DIR}/ansible"
 
-mkdir "$GIT_DIR"
-mkdir "$VENV_DIR"
+log() {
+  local level="${1^^}"
+  shift
+  echo "[$(date '+%F %T')] ${level} $@" >&2
+}
 
+INFO() {
+  log info "$@"
+}
+
+ERROR() {
+  log error "$@"
+}
+
+WARN() {
+  log warn "$@"
+}
+
+INFO creating directories
+mkdir -p "$GIT_DIR"
+mkdir -p "$VENV_DIR"
+mkdir -p "$BIN_DIR"
+mkdir -p "${BIN_DIR}/ansible"
+
+INFO install virtualenv
 sudo apt install virtualenv
-virtualenv "${VENV_DIR}"
+
+INFO install ansible
+virtualenv -p python2.7 "$ANSIBLE_DIR"
+$ANSIBLE_DIR/bin/pip install ansible
+
+INFO creating links for ansible under ~/bin
+for src in $(ls $ANSIBLE_DIR/bin/ansible*); do
+  name="$(basename "$src")"
+  link="${BIN_DIR}/ansible/${name}"
+  [[ -L "$link" ]] || ln -s -T "$src"
+done
+
 
 # vim: set ft=sh
 # vim: ft=sh
